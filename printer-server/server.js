445b598cf9f4d56a4dd224d5cd39f8c20a7b131d
emmanuelgruffy <emmanuelgruffy@EmmanueacStudio.lan>
2024-09-13 19:05:12 +0300

    fix filePath

--- printer-server/server.js
+++ printer-server/server.js
@@ -31,27 +31,24 @@ app.post("/api/print", upload.single("pdf"), (req, res) => {
   console.log(`Received PDF file at: ${pdfPath}`);
 
   // Step 1: Use the lp command to print the PDF file
-  exec(
-    `lp uploads/payment-receipt-template-farouk-musa.pdf`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error while printing: ${error.message}`);
-        return res
-          .status(500)
-          .send({ message: "Error while printing", error: error.message });
-      }
-      if (stderr) {
-        console.error(`Print stderr: ${stderr}`);
-        return res
-          .status(500)
-          .send({ message: "Error while printing", error: stderr });
-      }
-
-      // Print job succeeded
-      console.log(`Print stdout: ${stdout}`);
-      res.status(200).send({ message: "Print job sent successfully", stdout });
+  exec(`lp ${pdfPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error while printing: ${error.message}`);
+      return res
+        .status(500)
+        .send({ message: "Error while printing", error: error.message });
     }
-  );
+    if (stderr) {
+      console.error(`Print stderr: ${stderr}`);
+      return res
+        .status(500)
+        .send({ message: "Error while printing", error: stderr });
+    }
+
+    // Print job succeeded
+    console.log(`Print stdout: ${stdout}`);
+    res.status(200).send({ message: "Print job sent successfully", stdout });
+  });
 });
 
 app.listen(port, () => {
